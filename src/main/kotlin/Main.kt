607865ad5f0150c3fd0bd52ffc53fb28500704f3
artem <artemmrt@gmail.com>
2024-11-12 10:51:40 +0400

    KTB-02 initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 package org.example
 
+import java.io.File
+
 fun main() {
+
+    val wordsFile = File("words.txt")
+    val allWords = wordsFile.readLines()
+
+    allWords.forEach { println(it) }
 
 }
