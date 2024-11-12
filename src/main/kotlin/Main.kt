17fcf84165b32b74392de386c7ca63379879cd57
artem <artemmrt@gmail.com>
2024-11-12 15:28:34 +0400

    KTB-02 replaced with forEachLine

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,8 +5,6 @@ import java.io.File
 fun main() {
 
     val wordsFile = File("words.txt")
-    val allWords = wordsFile.readLines()
-
-    allWords.forEach { println(it) }
+    wordsFile.forEachLine { println(it) }
 
 }
